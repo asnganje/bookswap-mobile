264ef7815425798768379077737003c4a7cbbcd9
asnganje <abd.nganje@gmail.com>
2026-01-24 11:31:54 +0300

    add app wide state management

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import AppNavigator from './app/AppNavigator';
+import { Provider } from 'react-redux';
+import store from './store';
+import { useEffect, useState } from 'react';
+import { loadStoredUser } from './store/thunks/authThunk';
+import LoadingUI from './components/UI/Loading';
 
 export default function App() {
+  const [ready, setReady] =useState(false)
+
+  useEffect(()=> {
+    const loader = async() => {
+      await store.dispatch(loadStoredUser())
+    }
+    loader()
+    setReady(true)
+  }, [])
+
+  if (!ready) {
+    return <LoadingUI />
+  }
+
   return (
-    <View>
-      <StatusBar style="auto" />
-      <AppNavigator />
-    </View>
+    <Provider store={store}>
+      <View>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </View>
+    </Provider>
   );
 }
 
