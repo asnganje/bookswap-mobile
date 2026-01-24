937f1c9dcecbf6f9ad90fe3cc27b2e8b3f1ba7b4
asnganje <abd.nganje@gmail.com>
2026-01-24 11:30:43 +0300

    add auth thunk logic

---
+++ store/thunks/authThunk.ts
@@ -0,0 +1,51 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../api/client";
+import * as SecureStorage from "expo-secure-store"
+import { User } from "../../types/auth";
+
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async(userData, {rejectWithValue})=> {
+    try {
+      const response = await api.post("/users", userData, {
+        headers:{
+          "Content-Type":"multipart/form-data"
+        }
+      })
+      return response.data
+    } catch (error:any ) {
+      return rejectWithValue(error.response?.data?.error || "Registration failed!")
+    }
+  }
+)
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async(userData, {rejectWithValue})=> {
+    try {
+      const response = await api.post("users/sign_in", userData)
+      await SecureStorage.setItemAsync("token", response.data.token)
+      await SecureStorage.setItemAsync("user", JSON.stringify(response.data.user))
+      return response.data
+    } catch (error: any) {
+      return rejectWithValue(error.response?.data?.error || "Login failed!")
+    }
+  }
+)
+
+export const loadStoredUser = createAsyncThunk("auth/loadStoredUser", async()=> {
+  try {
+    const token = await SecureStorage.getItemAsync("token")
+    const userJson = await SecureStorage.getItemAsync("user")
+
+    if (token && userJson) {
+      return {
+        token,
+        user: JSON.parse(userJson) as User
+      }     
+    }
+    return null;
+  } catch (error) {
+    return null
+  }
+})
