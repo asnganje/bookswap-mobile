1e1b1a96c5b6cdae7a0fe3939bd1ee2d0fc7497a
asnganje <abd.nganje@gmail.com>
2026-01-27 15:04:34 +0300

    dynamically handle loading spinner props

--- components/UI/Loading.tsx
+++ components/UI/Loading.tsx
@@ -1,8 +1,8 @@
 import { ActivityIndicator, View } from "react-native";
 
-function LoadingUI() {
+function LoadingUI({color}: {color: string}) {
   return <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-      <ActivityIndicator size="large" color="#FFF"/>
+      <ActivityIndicator size="large" color={color}/>
     </View>
 }
 
