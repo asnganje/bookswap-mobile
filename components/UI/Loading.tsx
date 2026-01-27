d335cfd9a2afef78259fe757bdc665866faf4d72
asnganje <abd.nganje@gmail.com>
2026-01-27 08:13:57 +0300

    change styling for loadingUI

--- components/UI/Loading.tsx
+++ components/UI/Loading.tsx
@@ -2,7 +2,7 @@ import { ActivityIndicator, View } from "react-native";
 
 function LoadingUI() {
   return <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-      <ActivityIndicator size="large" color="#0000ff"/>
+      <ActivityIndicator size="large" color="#FFF"/>
     </View>
 }
 
