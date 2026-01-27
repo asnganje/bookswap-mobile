57d394babd78e86089bffe873169ab9c06ea2500
asnganje <abd.nganje@gmail.com>
2026-01-27 15:03:50 +0300

    dynamically display content inside root navigator

--- app/RootNavigator.tsx
+++ app/RootNavigator.tsx
@@ -1,13 +1,37 @@
-import { View } from "react-native"
 import { useSelector } from 'react-redux';
-import { RootState } from "../store";
+import { AppDispatch, RootState } from "../store";
 import AppNavigator from "./AppNavigator";
 import AuthNavigator from "./AuthNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { loadStoredUser } from '../store/thunks/authThunk';
+import LoadingUI from '../components/UI/Loading';
+import { Styles } from '../constants/colors';
+import { View } from 'react-native';
 
 
 function RootNavigator() {
   const { token } = useSelector((state: RootState)=>state.auth)
+  const dispatch = useDispatch<AppDispatch>()
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  useEffect(()=> {
+    dispatch(loadStoredUser())
+      .unwrap()
+      .finally(()=> {
+        setIsAuthenticated(true)
+      })
+  }, [dispatch])
+
+  if (!isAuthenticated) {
+    return (
+      <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+        <LoadingUI color={Styles.primary600}/>
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
       { token ? <AppNavigator /> : <AuthNavigator/> }
