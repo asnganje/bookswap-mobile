d153fe26d3d8da3c8c973a9ef6c7c225c1104d4a
asnganje <abd.nganje@gmail.com>
2026-02-14 10:14:02 +0300

    auto update loading UI in the root component

--- app/RootNavigator.tsx
+++ app/RootNavigator.tsx
@@ -1,43 +1,41 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store";
 import AuthNavigator from "./AuthNavigator";
 import { NavigationContainer } from "@react-navigation/native";
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { loadStoredUser } from '../store/thunks/authThunk';
-import LoadingUI from '../components/UI/Loading';
-import { Styles } from '../constants/colors';
-import { View } from 'react-native';
-import MainAppBottomTabsNavigator from './AppNavigator';
-
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { loadStoredUser } from "../store/thunks/authThunk";
+import LoadingUI from "../components/UI/LoadingUI";
+import { Styles } from "../constants/colors";
+import { View } from "react-native";
+import MainAppBottomTabsNavigator from "./AppNavigator";
 
 function RootNavigator() {
-  const { token } = useSelector((state: RootState)=>state.auth)
-  const dispatch = useDispatch<AppDispatch>()
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const { token } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch<AppDispatch>();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(loadStoredUser())
       .unwrap()
-      .finally(()=> {
-        setIsAuthenticated(true)
-      })
-  }, [dispatch])
+      .finally(() => {
+        setIsAuthenticated(true);
+      });
+  }, [dispatch]);
 
   if (!isAuthenticated) {
     return (
-      <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-        <LoadingUI color={Styles.primary600}/>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <LoadingUI color={Styles.primary600} />
       </View>
-    )
+    );
   }
 
   return (
     <NavigationContainer>
-      { token ? <MainAppBottomTabsNavigator /> : <AuthNavigator/> }
+      {token ? <MainAppBottomTabsNavigator /> : <AuthNavigator />}
     </NavigationContainer>
-  )  
+  );
 }
 
 export default RootNavigator;
-
