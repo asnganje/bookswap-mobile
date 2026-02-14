3363feee047d85c0cc9259a288190c26f9a2607a
asnganje <abd.nganje@gmail.com>
2026-02-14 10:14:56 +0300

    Add loading UI to booklist screen

--- screens/books/BooksListScreen.tsx
+++ screens/books/BooksListScreen.tsx
@@ -1,6 +1,25 @@
 import { Text, View } from "react-native";
+import { useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getBooks } from "../../store/thunks/booksThunk";
+import LoadingUI from "../../components/UI/LoadingUI";
+import { Styles } from "../../constants/colors";
 
 function BooksListScreen() {
+
+  const {books, loading} = useSelector((state: RootState)=>state.books)
+  const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(()=> {
+    dispatch(getBooks())
+  },[dispatch])
+
+  if (loading) {
+    return <LoadingUI color={Styles.primary600}/>
+  }
+  
   return(
     <View>
       <Text>BooksListScreen</Text>
