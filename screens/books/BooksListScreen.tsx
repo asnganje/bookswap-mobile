aee0db6006b83ec604bdc4dad89f5a236b3b5db0
asnganje <abd.nganje@gmail.com>
2026-02-14 11:33:45 +0300

    display bookcards in a flat list

--- screens/books/BooksListScreen.tsx
+++ screens/books/BooksListScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { useDispatch } from "react-redux";
@@ -6,25 +6,44 @@ import { useEffect } from "react";
 import { getBooks } from "../../store/thunks/booksThunk";
 import LoadingUI from "../../components/UI/LoadingUI";
 import { Styles } from "../../constants/colors";
+import { s, vs } from "react-native-size-matters";
+import BookCard from "../../components/BookCard";
 
 function BooksListScreen() {
+  const { books, loading } = useSelector((state: RootState) => state.books);
+  
+  const dispatch = useDispatch<AppDispatch>();
 
-  const {books, loading} = useSelector((state: RootState)=>state.books)
-  const dispatch = useDispatch<AppDispatch>()
-
-  useEffect(()=> {
-    dispatch(getBooks())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   if (loading) {
-    return <LoadingUI color={Styles.primary600}/>
+    return <LoadingUI color={Styles.primary600} />;
   }
-  
-  return(
-    <View>
-      <Text>BooksListScreen</Text>
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        contentContainerStyle={{
+          alignItems:"center",
+          justifyContent:"center",
+          rowGap:s(25),
+          paddingVertical:vs(20)
+        }
+        }
+        data={books}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <BookCard item={item} />}
+      />
     </View>
-  )
+  );
 }
 
 export default BooksListScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: s(15),
+  },
+});
