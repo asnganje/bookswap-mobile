d7d5117f065fa020ce028fbbf133ae7a907300d5
asnganje <abd.nganje@gmail.com>
2026-02-14 22:12:40 +0300

    add a reusable app Modal

--- components/AppModal.tsx
+++ components/AppModal.tsx
@@ -1,17 +1,22 @@
-import { ReactNode } from 'react'
-import { Modal, StyleSheet, Text} from 'react-native'
+import { ReactNode } from "react";
+import { Modal, StyleSheet, View } from "react-native";
+import { s } from "react-native-size-matters";
 
 interface ModalProps {
-  modalVisible: boolean
-  children: ReactNode
+  modalVisible: boolean;
+  children: ReactNode;
 }
 
-const AppModal = ({modalVisible, children}: ModalProps) => {
+const AppModal = ({ modalVisible, children }: ModalProps) => {
   return (
-      <Modal visible={modalVisible} animationType="slide">
-        {children}
-      </Modal>
-  )
-}
-export default AppModal
-const styles = StyleSheet.create({})
+    <Modal visible={modalVisible} animationType="slide">
+      <View style={styles.container}>{children}</View>
+    </Modal>
+  );
+};
+export default AppModal;
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: s(20),
+  },
+});
